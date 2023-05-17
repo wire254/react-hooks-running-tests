f8d3750165a76b1aea99cb772c33c32bc720eb90
Ulises <ulises.argueta95@gmail.com>
2023-05-17 17:27:41 -0500

    complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,10 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
